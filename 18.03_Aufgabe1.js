4a3636ba61a7bf899c9d0834b15f51a04fd9df4b
Spiitzii <spitzi@online.de>
2024-03-18 14:08:45 +0100

    HA18.03

---
+++ 18.03_Aufgabe1.js
@@ -0,0 +1,20 @@
+function bewertePunktzahl(punktzahl) {
+    if (punktzahl > 90) {
+        console.log("sehr gut");
+    } else if (punktzahl >= 80 && punktzahl <= 90) {
+        console.log("gut");
+    } else if (punktzahl >= 70 && punktzahl < 80) {
+        console.log("befriedigend");
+    } else if (punktzahl >= 60 && punktzahl < 70) {
+        console.log("ausreichend");
+    } else {
+        console.log("nicht bestanden");
+    }
+}
+
+
+bewertePunktzahl(91); // sehr gut
+bewertePunktzahl(81); // gut
+bewertePunktzahl(71); // befriedigend
+bewertePunktzahl(61); // ausreichend
+bewertePunktzahl(50); // nicht bestanden
---
+++ 18.03_Aufgabe2.js
@@ -0,0 +1,17 @@
+function Alter(alter) {
+    if (alter < 6) {
+        return "Die Fahrt ist kostenlos.";
+    } else if (alter >= 6 && alter <= 17) {
+        return "Die Fahrt ist zum Kinderpreis.";
+    } else if (alter >= 18 && alter <= 66) {
+        return "Die Fahrt ist zum Vollpreis.";
+    } else {
+        return "Die Fahrt ist zum Seniorenpreis.";
+    }
+}
+
+// Funktion testen, gib bei den Zahlen eine beliebige ein und bekomme das Ergebnis
+console.log(bewerteAlter(4));  //  Die Fahrt ist kostenlos.
+console.log(bewerteAlter(10)); //  Die Fahrt ist zum Kinderpreis.
+console.log(bewerteAlter(30)); //  Die Fahrt ist zum Vollpreis.
+console.log(bewerteAlter(70)); //  Die Fahrt ist zum Seniorenpreis.
